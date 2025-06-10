5433fc76eb9784898db833d0c7ff40faaf41fc64
BohdanSemenchenko <146064051+BohdanSemenchenko@users.noreply.github.com>
2025-06-10 22:51:26 +0300

    Add files via upload

---
+++ Lab_№3/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Bullet : Projectile
+    {
+        public Bullet(int damage) : base(damage) { }
+
+        public override void HitTarget(IDamageable target)
+        {
+            Console.WriteLine($"Bullet does {damage} damage");
+            target.TakeDamage(damage);
+        }
+    }
+}
---
+++ Lab_№3/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Enemy : IDamageable
+    {
+        public int Health { get; private set; }
+
+        public Enemy(int health)
+        {
+            Health = health;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            Console.WriteLine($"Enemy has taken {damage} damage. Left health: {Health}");
+            if (Health <= 0)
+            {
+                Console.WriteLine("Enemy killed");
+            }
+        }
+    }
+}
